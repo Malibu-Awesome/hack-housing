4ceface2ed03f22821cbf888e84ffe6a596ee432
Hector Martinez III <hmartineziii@gmail.com>
2015-02-08 11:22:13 -0800

    adding math and returning color code

--- api/routes/geoRoute.js
+++ api/routes/geoRoute.js
@@ -1,6 +1,19 @@
 'use strict';
 
 
+function calculateAffordability ( medianIncome, percentTransCost, houseBudget, transBudget){
+	var avgMoneySpentOnHT = medianIncome * percentTransCost/100;
+	console.log(medianIncome);
+	console.log(percentTransCost/100);
+	console.log(avgMoneySpentOnHT);
+	var inputMoneySpentOnHT = parseFloat(houseBudget) + parseFloat(transBudget);
+	console.log(inputMoneySpentOnHT);
+	if ((inputMoneySpentOnHT > 0.95*avgMoneySpentOnHT) && (inputMoneySpentOnHT < avgMoneySpentOnHT)) return {result: 'yellow'};
+	if (inputMoneySpentOnHT > avgMoneySpentOnHT) return {result: 'green'};
+	if (inputMoneySpentOnHT < 0.95*avgMoneySpentOnHT) return {result: 'red'};
+
+}
+
 module.exports.findAddress = function(app) {
 	var request = require('request');
 	var addressToGEOID;
@@ -15,30 +28,47 @@ module.exports.findAddress = function(app) {
 	};
 
 
-  	app.get('/api/v0/geo-route/:x/:y/:z', function (req, res) {
-	    addressString = 'http://geocoding.geo.census.gov/geocoder/geographies/address?street=' + req.params.x + 
-	    '&city=' + req.params.y + '&state=' + req.params.z + '&benchmark=Public_AR_Census2010&vintage=Census2010_Census2010&layers=14&format=json'
+  	app.get('/api/v0/geo-route/:a/:b/:c/:d/:e', function (req, res) {
+	    addressString = 'http://geocoding.geo.census.gov/' + 
+	    'geocoder/geographies/address?street=' + 
+	    req.params.a + 
+	    '&city=' + 
+	    req.params.b + 
+	    '&state=' + 
+	    req.params.c + 
+	    '&benchmark=Public_AR_Census2010&vintage=' +
+	    'Census2010_Census2010&layers=14&format=json'
 		
+	    var financialData = {
+	    	yearlyHousingBudget: req.params.d,
+	    	yearlyTransCostBudget: req.params.e
+	    }
+
 		var options = {
 			url: addressString,
 			method: 'GET',
 			headers: headers
-		};	
+		};
 		
 		request(options, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				addressToGEOID = JSON.parse(body).result.addressMatches[0].geographies["Census Blocks"][0].GEOID.slice(0,12);
 			}
 			var affordabilityOptions = {
-				url: 'http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/LocationAffordabilityIndexData/FeatureServer/0/query?where=blkgrp%3D' + addressToGEOID + '&outFields=hh_type1_ht_own%2C+area_median_income&f=json'
+				url: 'http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/LocationAffordabilityIndexData/FeatureServer/0/query?where=blkgrp%3D' + addressToGEOID + '&outFields=hh_type1_ht_own%2C+blkgrp_median_income_owners&f=json'
 			}
 			request(affordabilityOptions, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					affordabilityResult = JSON.parse(body);
+					var blkgrpMedianIncome = affordabilityResult.features[0].attributes.blkgrp_median_income_owners;
+					var houseAndTransPercentageOfIncome = affordabilityResult.features[0].attributes.hh_type1_ht_own;
 				}
-				res.send({
-		    		result: affordabilityResult
-		    	})
+
+				var colorMeAffordable = calculateAffordability(blkgrpMedianIncome
+					, houseAndTransPercentageOfIncome
+					, financialData.yearlyHousingBudget
+					, financialData.yearlyTransCostBudget)
+				res.send(colorMeAffordable)
 	    });
 		})
 	});
