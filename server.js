0dd20bf8171959f8955f247a05c736cfb56dcdca
Hailey Petway <hpetway@BELC02N31GTFD58.sea.corp.expecn.com>
2015-02-07 21:01:15 -0800

    commiting first socket changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var http = require("http");
+var io = require('socket.io')(http);
 
 //SERVER SETUP
 var app = express();
@@ -11,6 +12,19 @@ server.listen(app.get("port"), function () {
     console.log("Server start on:" + app.get("port"));
 });
 //END SERVER SETUP
+
+//SOCKET.IO SETUP
+app.get('/', function(req, res){
+  res.sendfile('app/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
 
 //ROUTES
 require("./api/routes/oneRoute")(app);
