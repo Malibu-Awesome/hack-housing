2feedfe990b20528b9ea3ad2d029fc8ed356bd49
Hector Martinez III <hmartineziii@gmail.com>
2015-04-04 07:09:44 -0700

    reorganized server.js

--- server.js
+++ server.js
@@ -3,29 +3,28 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var app = express();
 
-// setup transparent jsx requires
+//SETUP TRANSPARENT JSX REQUIRES
 require('node-jsx').install({extension: ['js']});
 
 //SERVER SETUP
-
 app.set("port", process.env.PORT || 4000);
-
 app.use(".hbs", exphbs({ extname: ".hbs"}));
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "/templates"));
 
-
 //STATIC ROUTES
 app.use(express.static(__dirname + "/dist"));
 
-//ROUTES
+//ENTRY ROUTE
 app.get("/", function (req, res) {
   return res.render("index", {});
 });
 
+//API ROUTES
 require("./api/routes/oneRoute")(app);
 require("./api/routes/geoRoute").findAddress(app);
 
+//START SERVER
 app.listen(app.get("port"), function () {
     console.log("Server start on:" + app.get("port"));
 });
